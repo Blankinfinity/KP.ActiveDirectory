f7fe4767dd9660dccbc0671e31931cd700e27ec3
Keith <Keith@K0007005>
2017-01-07 23:46:23 +0000

    Added ADConnect and ADUsers classes. Methods added to ADConnect to create a connection and secure connection to AD.

---
+++ KP.ActiveDirectory/ADConnect.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KP.ActiveDirectory
+{
+    class ADConnect
+    {
+        /// <summary>
+        /// Method used to create an entry to the AD.
+        /// Replace the path, username, and password.
+        /// </summary>
+        /// <returns>DirectoryEntry</returns>
+        public static DirectoryEntry GetDirectoryEntry()
+        {
+            DirectoryEntry de = new DirectoryEntry();
+            de.Path = "LDAP://192.168.1.1/CN=Users;DC=Yourdomain";
+            de.Username = @"yourdomain\sampleuser";
+            de.Password = "samplepassword";
+            return de;
+        }
+
+        /// <summary>
+        /// Method used to create an entry to the AD using a secure connection.
+        /// Replace the path.
+        /// </summary>
+        /// <returns>DirectoryEntry</returns>
+        public static DirectoryEntry GetSecureDirectoryEntry()
+        {
+            DirectoryEntry de = new DirectoryEntry();
+            de.Path = "LDAP://192.168.1.1/CN=Users;DC=Yourdomain";
+            de.AuthenticationType = AuthenticationTypes.Secure;
+            return de;
+        }
+
+    }
+}
