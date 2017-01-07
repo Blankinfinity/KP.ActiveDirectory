3797c8df5916b6e1a62313db5f9ee20a51b88766
Keith <Keith@K0007005>
2017-01-07 23:39:03 +0000

    Added reference to DirectoryServices. Also added Domain Controllers class.

---
+++ KP.ActiveDirectory/DomainControllers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.DirectoryServices;
+
+namespace KP.ActiveDirectory
+{
+    class DomainControllers
+    {
+        public List<string> GetDomainControllers()
+        {
+            List<string> DcList = new List<string>();
+
+            using (DirectorySearcher Searcher = new DirectorySearcher(new DirectoryEntry()))
+            {
+                Searcher.Filter = "(primaryGroupID=516)";
+                foreach (SearchResult DC in Searcher.FindAll())
+                {
+                    DcList.Add(DC.GetDirectoryEntry().Name.Remove(0, 3));
+                }
+            }
+            return DcList;
+        }
+
+    }
+}
